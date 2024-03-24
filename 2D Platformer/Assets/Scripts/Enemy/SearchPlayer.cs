07ded35fa37d01df78b69e44578027a27f0fcabb
SrNeox <gsabinskij33@gmail.com>
2024-03-24 16:58:45 +0300

    Refactoring

---
+++ 2D Platformer/Assets/Scripts/Enemy/SearchPlayer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class SearchPlayer : MonoBehaviour
+{
+    [SerializeField] private EnemyMover _enemyMover;
+    [SerializeField] private float _speed;
+
+    private PlayerMover _player;
+
+    private void Update()
+    {
+        if (_player != null)
+            MoveToPlayer();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.TryGetComponent(out _player))
+        {
+            _enemyMover.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.TryGetComponent(out _player))
+        {
+            _enemyMover.enabled = true;
+            _player = null;
+        }
+    }
+
+    private void MoveToPlayer()
+    {
+        _enemyMover.transform.position = Vector2.MoveTowards
+            (_enemyMover.transform.position, _player.transform.position, _speed * Time.deltaTime);
+    }
+}
